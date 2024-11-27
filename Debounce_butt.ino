94f975aecf9417fd59c492d34efc4f40e8929577
Harshul8824 <goswamiharshul88@gmail.com>
2024-11-27 23:54:17 +0530

    add the new mini project of arduino

--- Debounce_butt.ino
+++ Debounce_butt.ino
@@ -1,48 +1,53 @@
-//this is best appproach
+// this is best appproach
 const int led = 5;
 const int butt = 4;
 int buttState;
-int lastButtState = HIGH; 
-int c = 0;              
-unsigned long lastDebounceTime = 0;  
-unsigned long debounceDelay = 50;  
+int lastButtState = HIGH;
+int c = 0;
+unsigned long lastDebounceTime = 0;
+unsigned long debounceDelay = 50;
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
   pinMode(led, OUTPUT);
   pinMode(butt, INPUT_PULLUP);
 }
 
-void loop() {
+void loop()
+{
   int reading = digitalRead(butt);
 
-  if (reading != lastButtState) {
-    lastDebounceTime = millis(); 
+  if (reading != lastButtState)
+  {
+    lastDebounceTime = millis();
   }
-//with the help of millis this if condition is true at every 0.05 sec after every debounce
-  if ((millis() - lastDebounceTime) > debounceDelay) {
-    if (reading == LOW && buttState == HIGH) {
-      c=c==0? 1:0;
+  // with the help of millis this if condition is true at every 0.05 sec after every debounce
+  if ((millis() - lastDebounceTime) > debounceDelay)
+  {
+    if (reading == LOW && buttState == HIGH)
+    {
+      c = c == 0 ? 1 : 0;
     }
     buttState = reading;
   }
   digitalWrite(led, c);
-  lastButtState = reading; 
+  lastButtState = reading;
 }
 
-//bad practice
-// const int led=5;
-// const int butt=4;
-// int buttState;
-// int c=0;
-// void setup() {
-//   Serial.begin(115200);
-//   pinMode(led, OUTPUT);
-//   pinMode(butt, INPUT_PULLUP);
-// }
+// bad practice
+//  const int led=5;
+//  const int butt=4;
+//  int buttState;
+//  int c=0;
+//  void setup() {
+//    Serial.begin(115200);
+//    pinMode(led, OUTPUT);
+//           pinMode(butt, INPUT_PULLUP);
+//  }
 
 // void loop() {
-//   buttState=digitalRead(butt);
+//       buttState=digitalRead(butt);
 // if(buttState==LOW){
 //   c=c==0?1:0;
 // }
